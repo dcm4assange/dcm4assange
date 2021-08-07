3b943f2845139264e1b9325dab0f8266dd91db81
Gunter Zeilinger <gunterze@protonmail.com>
2021-08-07 18:36:24 +0200

    FileBulkDataElement.writeValueTo()

--- core/src/main/java/org/dcm4assange/FileBulkDataElement.java
+++ core/src/main/java/org/dcm4assange/FileBulkDataElement.java
@@ -14,18 +14,18 @@ import java.util.Optional;
  */
 class FileBulkDataElement extends BasicDicomElement {
     private final Path file;
-    private final long valuePos;
+    private final long offset;
 
-    FileBulkDataElement(Path file, long valuePos, DicomElement dcmElm) {
+    FileBulkDataElement(Path file, long offset, DicomElement dcmElm) {
         super(dcmElm.containedBy(), dcmElm.tag(), dcmElm.vr(), dcmElm.valueLength());
         this.file = file;
-        this.valuePos = valuePos;
+        this.offset = offset;
     }
 
     @Override
     public Optional<String> bulkDataURI() {
         StringBuilder sb = new StringBuilder(file.toUri().toString())
-                .append("#offset=").append(valuePos);
+                .append("#offset=").append(offset);
         if (valueLength == -1)
             sb.append(",length=-1");
         else
@@ -37,21 +37,21 @@ class FileBulkDataElement extends BasicDicomElement {
     public void writeValueTo(DicomOutputStream dos) throws IOException {
         byte[] buffer = new byte[8192];
         try (InputStream in = Files.newInputStream(file)) {
-            transfer(in, valuePos, buffer, null);
+            transferTo(in, offset, buffer, null);
             if (valueLength != -1) {
-                transfer(in, valueLength & 0xffffffffL, buffer, dos);
+                transferTo(in, valueLength & 0xffffffffL, buffer, dos);
             } else {
                 in.read(buffer, 0, 8);
                 while (ByteOrder.LITTLE_ENDIAN.bytesToTag(buffer, 0) == Tag.Item) {
                     dos.write(buffer, 0, 8);
-                    transfer(in, ByteOrder.LITTLE_ENDIAN.bytesToInt(buffer, 4) & 0xffffffffL, buffer, dos);
+                    transferTo(in, ByteOrder.LITTLE_ENDIAN.bytesToInt(buffer, 4) & 0xffffffffL, buffer, dos);
                     in.read(buffer, 0, 8);
                 }
             }
         }
     }
 
-    private void transfer(InputStream in, long length, byte[] buffer, OutputStream out) throws IOException {
+    private void transferTo(InputStream in, long length, byte[] buffer, OutputStream out) throws IOException {
         int read;
         while (length > 0 && (read = in.read(buffer, 0, (int) Math.min(length, buffer.length))) >= 0) {
             if (out != null) out.write(buffer, 0, read);
